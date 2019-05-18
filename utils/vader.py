5b6a693b19b5ce79f862a2246c1ce349273a97ea
Claire Herdeman <cherdeman@uchicago.edu>
2019-05-18 15:19:59 -0500

    starting vader analysis

---
+++ utils/vader.py
@@ -0,0 +1,26 @@
+from utils.db_client import DBClient
+import pandas as pd
+
+
+class VADER_analyzer:
+    def __init__(self, topic):
+        self.topic = topic
+        self.query = """
+                    SELECT tweet_id, tweet_date, tweet_text_raw, user_id, democrat, leadership
+                    from staging.master
+                    where {} = 1
+                    """
+        self.df = None
+
+
+    def get_data(self):
+        db = DBClient()
+        q = self.query
+        self.df = pd.DataFrame(db.read(q.format(self.topic)))
+
+
+    def run_vader(self):
+        pass
+
+    def outcome_by_group(self):
+        pass
